214d1419c4559d57996ff1146932eb57ce4c0252
MaxAdmin <maxeduit2@gmail.com>
2022-03-31 18:14:58 +0300

    07_wall.py is EDIT

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -6,21 +6,16 @@ import simple_draw
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-def brick():
-    for _ in range(1):
-        simple_draw.line(start_point=point_1, end_point=point_2, color=simple_draw.COLOR_DARK_YELLOW, width=1)
-
-
 for y in range(0, 700, 50):
     for x in range(0, 700, 100):
         point_1 = simple_draw.get_point(x, y)
         point_2 = simple_draw.get_point(x, y+50)
-        brick()
+        simple_draw.line(start_point=point_1, end_point=point_2, color=simple_draw.COLOR_DARK_YELLOW, width=1)
 
 for x in range(0, 700, 100):
     for y in range(0, 700, 50):
         point_1 = simple_draw.get_point(x, y)
         point_2 = simple_draw.get_point(x+100, y)
-        brick()
+        simple_draw.line(start_point=point_1, end_point=point_2, color=simple_draw.COLOR_DARK_YELLOW, width=1)
 
 simple_draw.pause()
