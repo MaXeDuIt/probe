97015a288414c5cb27999e251d3b6f4597793e1d
MaxAdmin <maxeduit2@gmail.com>
2022-03-31 18:33:26 +0300

    08_smile.py is DOING

--- lesson_003/08_smile.py
+++ lesson_003/08_smile.py
@@ -8,6 +8,29 @@ import simple_draw
 # Параметры функции: кордината X, координата Y, цвет.
 # Вывести 10 смайликов в произвольных точках экрана.
 
-# TODO здесь ваш код
+def smile(x, y):
+    point = simple_draw.get_point(x, y)
+    radius = 50
+    simple_draw.circle(center_position=point, radius=radius, width=2)
+    point = simple_draw.get_point(x + 15, y + 20)
+    radius = 5
+    simple_draw.circle(center_position=point, radius=radius, width=2)
+    point = simple_draw.get_point(x - 15, y + 20)
+    radius = 5
+    simple_draw.circle(center_position=point, radius=radius, width=2)
+
+
+
+
+
+#    def bubble(point, step):
+#        radius = 50
+#        for _ in range(3):
+#            radius += step
+#            sd.circle(center_position=point, radius=radius, width=2)
+#
+#    point = sd.get_point(300, 300)
+#    bubble(point=point, step=10)
+smile(100, 100)
 
 simple_draw.pause()
