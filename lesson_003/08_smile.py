2219032f9ee5eb1d27316bcc401471d4e1fa3fd3
MaxAdmin <maxeduit2@gmail.com>
2022-04-01 13:09:21 +0300

    08_smile.py is DONE

--- lesson_003/08_smile.py
+++ lesson_003/08_smile.py
@@ -1,36 +1,41 @@
 # -*- coding: utf-8 -*-
 
 # (определение функций)
-import simple_draw
+import simple_draw as sd
 
 # Написать функцию отрисовки смайлика в произвольной точке экрана
 # Форма рожицы-смайлика на ваше усмотрение
 # Параметры функции: кордината X, координата Y, цвет.
 # Вывести 10 смайликов в произвольных точках экрана.
 
-def smile(x, y):
-    point = simple_draw.get_point(x, y)
-    radius = 50
-    simple_draw.circle(center_position=point, radius=radius, width=2)
-    point = simple_draw.get_point(x + 15, y + 20)
-    radius = 5
-    simple_draw.circle(center_position=point, radius=radius, width=2)
-    point = simple_draw.get_point(x - 15, y + 20)
-    radius = 5
-    simple_draw.circle(center_position=point, radius=radius, width=2)
+def smile(x, y, color):
+    start_point = sd.get_point(x, y)
 
+    sd.circle(start_point, 50, color=color, width=3)
 
+    sd.circle(sd.get_point(x + 15, y + 20), 5, color=color, width=3)
 
+    sd.circle(sd.get_point(x - 15, y + 20), 5, color=color, width=3)
 
+    sd.line(sd.get_point(x, y + 5), sd.get_point(x, y - 10), color=color, width=3)
 
-#    def bubble(point, step):
-#        radius = 50
-#        for _ in range(3):
-#            radius += step
-#            sd.circle(center_position=point, radius=radius, width=2)
-#
-#    point = sd.get_point(300, 300)
-#    bubble(point=point, step=10)
-smile(100, 100)
+    sd.line(sd.get_point(x, y + 40), sd.get_point(x, y + 80), color=color, width=3)
 
-simple_draw.pause()
+    sd.line(sd.get_point(x - 15, y + 40), sd.get_point(x - 30, y + 80), color=color, width=3)
+
+    sd.line(sd.get_point(x + 15, y + 40), sd.get_point(x + 30, y + 80), color=color, width=3)
+
+    sd.line(sd.get_point(x - 10, y - 25), sd.get_point(x + 10, y - 25), color=color, width=3)
+
+    sd.line(sd.get_point(x - 10, y - 25), sd.get_point(x - 20, y - 20), color=color, width=3)
+
+    sd.line(sd.get_point(x + 10, y - 25), sd.get_point(x + 20, y - 20), color=color, width=3)
+
+for i in range(10):
+    point = sd.random_point()
+    x = point.x
+    y = point.y
+    color = sd.random_color()
+    smile(x, y, color)
+
+sd.pause()
