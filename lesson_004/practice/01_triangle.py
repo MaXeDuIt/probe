ed478e2f504a4eec1daed34ed87422a347cc2b34
MaxAdmin <maxeduit2@gmail.com>
2022-04-04 16:54:36 +0300

    Practice is DONE

--- lesson_004/practice/01_triangle.py
+++ lesson_004/practice/01_triangle.py
@@ -28,7 +28,6 @@ def triangle(point, angle=0):
     v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 240, length=200, width=3)
     v3.draw()
 
-
 point_0 = sd.get_point(300, 300)
 
 for angle in range(0, 361, 30):
--- lesson_004/practice/02_fractal.py
+++ lesson_004/practice/02_fractal.py
@@ -9,6 +9,10 @@ sd.resolution = (600, 800)
 point_0 = sd.get_point(300, 5)
 
 
+
+# v1 = sd.get_vector(start_point=point_0, angle=90, length=100)
+# v1.draw()
+
 # сделать функцию рисования ветки из заданной точки,
 # заданной длины, с заданным наклоном
 # def branch(point, angle, length):
@@ -20,13 +24,18 @@ point_0 = sd.get_point(300, 5)
 # length_0 = 200
 # next_point = branch(point=point_0, angle=angle_0, length=length_0)
 # next_angle = angle_0 - 30
-# next_length = length_0 * .75
+# next_length = length_0 * 0.75
 # next_point = branch(point=next_point, angle=next_angle, length=next_length)
 # next_angle = next_angle - 30
-# next_length = next_length * .75
+# next_length = next_length * 0.75
+# next_point = branch(point=next_point, angle=next_angle, length=next_length)
+# next_angle = next_angle - 30
+# next_length = next_length * 0.75
 # next_point = branch(point=next_point, angle=next_angle, length=next_length)
 
+
 # написать цикл рисования ветвей с постоянным уменьшением длины на 25% и отклонением на 30 градусов
+
 # angle_0 = 90
 # length_0 = 200
 #
@@ -37,7 +46,8 @@ point_0 = sd.get_point(300, 5)
 # while next_length > 1:
 #     next_point = branch(point=next_point, angle=next_angle, length=next_length)
 #     next_angle = next_angle - 30
-#     next_length = next_length * .75
+#     next_length = next_length * 0.75
+
 
 # сделать функцию branch рекурсивной
 
@@ -48,15 +58,13 @@ def branch(point, angle, length, delta):
     v1.draw()
     next_point = v1.end_point
     next_angle = angle - delta
-    next_length = length * .75
+    next_length = length * 0.75
     branch(point=next_point, angle=next_angle, length=next_length, delta=delta)
-
 
 for delta in range(0, 51, 10):
     branch(point=point_0, angle=90, length=150, delta=delta)
 for delta in range(-50, 1, 10):
     branch(point=point_0, angle=90, length=150, delta=delta)
-
 
 sd.pause()
 
