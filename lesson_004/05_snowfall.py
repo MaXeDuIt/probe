856eb513050e25977e28e7639b4485d9bfe0fc7b
MaxAdmin <maxeduit2@gmail.com>
2022-04-07 16:40:01 +0300

    05_snowfall.py is PROCESS

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -2,13 +2,13 @@
 
 import simple_draw as sd
 
-
+sd.resolution = (1200, 600)
 # На основе кода из практической части реализовать снегопад:
 # - создать списки данных для отрисовки N снежинок
 # - нарисовать падение этих N снежинок
 # - создать список рандомных длинн лучей снежинок (от 10 до 100) и пусть все снежинки будут разные
 
-N = 20
+N = 5
 
 # Пригодятся функции
 # sd.get_point()
@@ -17,17 +17,43 @@ N = 20
 # sd.random_number()
 # sd.user_want_exit()
 
-# TODO здесь ваш код
-while True:
-    sd.clear_screen()
-    pass
-    pass
-    pass
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
+for element in range(N):
+    x = sd.random_number(100, 1100)
+    y = sd.random_number(400, 550)
+    element = (x, y)
+    coordinates = [element]
+    print(coordinates)
+    for x, y in coordinates:
+        length = sd.random_number(10, 100)
+        sd.start_drawing()
+        point = sd.get_point(x, y)
+        sd.snowflake(center=point, length=length)
+        while True:
+            point = sd.get_point(x, y)
+            sd.snowflake(center=point, length=length, color=sd.background_color)
+            y -= 20
+            x += sd.random_number(-25, 25)
+            point = sd.get_point(x, y)
+            sd.snowflake(center=point, length=length, color=sd.COLOR_WHITE)
+            if y < 50:
+                break
+            sd.finish_drawing()
+            sd.sleep(0.1)
+            if sd.user_want_exit():
+                break
 
 sd.pause()
+
+#             sd.clear_screen()
+#             point = sd.get_point(x, y)
+#             sd.snowflake(center=point, length=length)
+#             y -= 25
+#             if y < 50:
+#                 break
+#             x += sd.random_number(-20, 20)
+#             sd.sleep(0.1)
+#             if sd.user_want_exit():
+#                 break
 
 # подсказка! для ускорения отрисовки можно
 #  - убрать clear_screen()
@@ -43,5 +69,3 @@ sd.pause()
 # - сделать сугоб внизу экрана - если снежинка долетает до низа, оставлять её там,
 #   и добавлять новую снежинку
 # Результат решения см https://youtu.be/XBx0JtxHiLg
-
-
