ca9dde17a6d3ba15108ddb6b9ca69900df15d3b7
MaxAdmin <maxeduit2@gmail.com>
2022-04-11 17:46:12 +0300

    03_burger.py is DONE

---
+++ lesson_005/my_burger.py
@@ -0,0 +1,21 @@
+
+def bun():
+    print('Начнем с булочек')
+
+def cutlet():
+    print('Кладем котлеты')
+
+def cucumber():
+    print('А теперь добавим огурчики')
+
+def tomato():
+    print('Добавим помидоры')
+
+def mayonnaise():
+    print('Смажем майонезем')
+
+def cheese():
+    print('Кладем сыр')
+
+def salad():
+    print('Не забываем про салат')
