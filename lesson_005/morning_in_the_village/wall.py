d2a17a7900c80d5c4b29ac32bfe95a25d919888a
MaxAdmin <maxeduit2@gmail.com>
2022-04-13 11:17:05 +0300

    village in progress

--- lesson_005/morning_in_the_village/wall.py
+++ lesson_005/morning_in_the_village/wall.py
@@ -9,16 +9,40 @@ def wall():
             point_1 = sd.get_point(x, y)
             point_2 = sd.get_point(x, y + 50)
             sd.line(start_point=point_1, end_point=point_2, color=sd.COLOR_GREEN, width=1)
+    for y in range(25, 225, 100):
+        for x in range(550, 700, 100):
+            point_1 = sd.get_point(x, y)
+            point_2 = sd.get_point(x, y + 50)
+            sd.line(start_point=point_1, end_point=point_2, color=sd.COLOR_GREEN, width=1)
+    for y in range(75, 225, 100):
+        for x in range(450, 700, 100):
+            point_1 = sd.get_point(x, y)
+            point_2 = sd.get_point(x, y + 50)
+            sd.line(start_point=point_1, end_point=point_2, color=sd.COLOR_GREEN, width=1)
 
-    # for y in range(50, 700, 100):
-    #     for x in range(50, 700, 100):
-    #         point_1 = sd.get_point(x, y)
-    #         point_2 = sd.get_point(x, y + 50)
-    #         sd.line(start_point=point_1, end_point=point_2, color=sd.COLOR_GREEN, width=1)
-    #
-    # for x in range(0, 700, 100):
-    #     for y in range(0, 700, 50):
-    #         point_1 = sd.get_point(x, y)
-    #         point_2 = sd.get_point(x + 100, y)
-    #         sd.line(start_point=point_1, end_point=point_2, color=sd.COLOR_GREEN, width=1)
+    for x in range(400, 550, 50):
+        for y in range(25, 226, 50):
+            point_1 = sd.get_point(x, y)
+            point_2 = sd.get_point(x + 50, y)
+            sd.line(start_point=point_1, end_point=point_2, color=sd.COLOR_GREEN, width=1)
+    for x in range(650, 700, 50):
+        for y in range(25, 226, 50):
+            point_1 = sd.get_point(x, y)
+            point_2 = sd.get_point(x + 50, y)
+            sd.line(start_point=point_1, end_point=point_2, color=sd.COLOR_GREEN, width=1)
 
+    for y in range(225, 325, 100):
+        for x in range(400, 700, 100):
+            point_1 = sd.get_point(x, y)
+            point_2 = sd.get_point(x, y + 50)
+            sd.line(start_point=point_1, end_point=point_2, color=sd.COLOR_GREEN, width=1)
+    for y in range(275, 325, 100):
+        for x in range(450, 700, 100):
+            point_1 = sd.get_point(x, y)
+            point_2 = sd.get_point(x, y + 50)
+            sd.line(start_point=point_1, end_point=point_2, color=sd.COLOR_GREEN, width=1)
+    for x in range(400, 700, 50):
+        for y in range(225, 326, 50):
+            point_1 = sd.get_point(x, y)
+            point_2 = sd.get_point(x + 50, y)
+            sd.line(start_point=point_1, end_point=point_2, color=sd.COLOR_GREEN, width=1)
