353de05109ed6489179b584687752609228c904f
MaxAdmin <maxeduit2@gmail.com>
2022-04-14 13:53:32 +0300

    First version of game "nim"

--- lesson_006/python_snippets/03_practice_faqs.py
+++ lesson_006/python_snippets/03_practice_faqs.py
@@ -9,12 +9,12 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
 ##########
 # Нарисовать радугу: 7 линий толщиной 4 с шагом 5 из точки (50, 50) в точку (550, 550)
 
-x, y = 50, 50
-for color in rainbow_colors:
-    start_point = sd.get_point(x, y)
-    end_point = sd.get_point(x + 500, y + 500)
-    sd.line(start_point=start_point, end_point=end_point, color=color, width=4)
-    x += 5
+# x, y = 50, 50
+# for color in rainbow_colors:
+#     start_point = sd.get_point(x, y)
+#     end_point = sd.get_point(x + 500, y + 500)
+#     sd.line(start_point=start_point, end_point=end_point, color=color, width=4)
+#     x += 5
 
 
 # width = 4
@@ -33,7 +33,7 @@ for color in rainbow_colors:
 # for y in range(0, sd.resolution[1], brick_y):
 #     row += 1
 #     for x in range(0, sd.resolution[0], brick_x):
-#         x0 = x if row % 2 else x + brick_x // 3
+#         x0 = x if row % 2 else x + brick_x // 2
 #         left_bottom = sd.get_point(x0, y)
 #         right_top = sd.get_point(x0 + brick_x, y + brick_y)
 #         sd.rectangle(left_bottom=left_bottom, right_top=right_top, width=1)
--- lesson_006/python_snippets/04_practice.py
+++ lesson_006/python_snippets/04_practice.py
@@ -32,14 +32,11 @@ while True:
     cprint(get_bunches(), color='green')
     user_color = 'blue' if user_number == 1 else 'yellow'
     cprint('Ход игрока {}'.format(user_number), color=user_color)
-    pos = input(colored('Откуда берем?', color=user_color))
-    qua = input(colored('Сколько берем?', color=user_color))
-    step_successed = take_from_bunch(position=int(pos), quantity=int(qua))
-    if step_successed:
-        user_number = 2 if user_number == 1 else 1
-    else:
-        cprint('Невозможный ход!', color='red')
+    pos = input(colored('Откуда берём?', color=user_color))
+    qua = input(colored('Сколько берём?', color=user_color))
+    take_from_bunch(position=int(pos), quantity=int(qua))
     if is_gameover():
         break
+    user_number = 2 if user_number == 1 else 1
 
 cprint('Выйграл игрок номер {}'.format(user_number), color='red')
