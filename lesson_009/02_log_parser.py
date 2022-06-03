3163fade31d7ae533805e582ede3370d1451d4d8
MaxAdmin <maxeduit2@gmail.com>
2022-06-03 17:00:53 +0300

    Lesson_009 01_char_stat.py Step #2 - перенос кода на классы

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -19,27 +19,51 @@
 # Входные параметры: файл для анализа, файл результата
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
+class Parser:
 
-file_name_in = 'events.txt'
-file_name_out = '02_out.txt'
-prev_line = ''
-count = 1
-with open(file_name_in, 'r') as file_in:
-    with open(file_name_out, 'w') as file_out:
-        for line in file_in:
-            line = line[:-1]
-            if line.endswith('NOK'):
-                line = line[1:17]
-                if line == prev_line:
-                    count += 1
+    def __init__(self, file_name_in, file_name_out):
+        self.file_name_in = file_name_in
+        self.file_name_out = file_name_out
+        self.file_in = None
+        self.file_out = None
+        self.line = None
+        self.prev_line = ''
+        self.line_for_write = ''
+        self.count = 1
+
+    def file_open(self):
+        self.file_in = open(self.file_name_in, 'r')
+        self.file_out = open(self.file_name_out, 'w')
+        return self.file_in, self.file_out
+
+    def close_file(self):
+        self.file_in.close()
+        self.file_out.close()
+
+    def write_file(self):
+        self.line_for_write = f"{self.prev_line} {self.count}\n"
+        self.file_out.write(self.line_for_write)
+
+    def act(self):
+        for self.line in self.file_in:
+            self.line = self.line[:-1]
+            if self.line.endswith('NOK'):
+                self.line = self.line[1:17]
+                if self.line == self.prev_line:
+                    self.count += 1
                 else:
-                    if prev_line:
-                        line_for_write = f"{prev_line} {count}\n"
-                        file_out.write(line_for_write)
-                        print(prev_line, count)
-                        count = 1
-                prev_line = line
+                    if self.prev_line:
+                        line_for_write = f"{self.prev_line} {self.count}\n"
+                        self.file_out.write(line_for_write)
+                        print(self.prev_line, self.count)
+                        self.count = 1
+                self.prev_line = self.line
 
+
+parser = Parser(file_name_in='events.txt', file_name_out='02_out.txt')
+parser.file_open()
+parser.act()
+parser.close_file()
 
 
 
