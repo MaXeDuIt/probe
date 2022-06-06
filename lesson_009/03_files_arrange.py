af4b82981029b4871d422deb258c81ab2304d3b1
MaxAdmin <maxeduit2@gmail.com>
2022-06-06 16:24:00 +0300

    Lesson_009 03_files_arrange.py.py Step #1 - создан класс Arrange и методы поиска файлов в папке icons - определено время создания файла

--- lesson_009/03_files_arrange.py
+++ lesson_009/03_files_arrange.py
@@ -34,7 +34,65 @@ import os, time, shutil
 # Чтение документации/гугла по функциям - приветствуется. Как и поиск альтернативных вариантов :)
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
-# TODO здесь ваш код
+import time
+import os
+import zipfile
+
+
+class Arrange:
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.file_time = None
+
+    def search(self):
+        for dirpath, dirnames, filenames in os.walk(self.file_name):
+            for file in filenames:
+                full_file_path = os.path.join(dirpath, file)
+                secs = os.path.getmtime(full_file_path)
+                self.file_time = time.gmtime(secs)
+                year = self.file_time[0]
+                month = self.file_time[1]
+                self.creation_dir(folder=year, subfolder=month)
+
+    def creation_dir(self, folder, subfolder):
+        self.folder = folder
+        self.subfolder = subfolder
+        os.makedirs(f"icons_by_year\\{self.folder}\\{self.subfolder}")
+
+icons = Arrange('icons')
+icons.search()
+
+
+
+# zfile = zipfile.ZipFile('icons.zip', 'r')
+# for file_info in zfile.infolist():
+#     print(file_info.filename, file_info.date_time, file_info.file_size)
+
+
+
+
+# path = 'C:/Windows/help'
+# path_normalized = os.path.normpath(path)
+# print(path_normalized)
+#
+# count = 0
+# for dirpath, dirnames, filenames in os.walk(path_normalized):
+#     print('*' * 27)
+#     print(dirpath, dirnames, filenames)
+#     print(os.path.dirname(dirpath))
+#     count += len(filenames)
+#     for file in filenames:
+#         full_file_path = os.path.join(dirpath, file)
+#         secs = os.path.getmtime(full_file_path)
+#         file_time = time.gmtime(secs)
+#         if file_time[0] == 2013:
+#             # выводим только файлы за 2013 год
+#             print(full_file_path, secs, file_time)
+# print(count)
+#
+# print(__file__, os.path.dirname(__file__))
+
 
 # Усложненное задание (делать по желанию)
 # Нужно обрабатывать zip-файл, содержащий фотографии, без предварительного извлечения файлов в папку.
