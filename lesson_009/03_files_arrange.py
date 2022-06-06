1d2b03574cad13f25bb94e6c40df8e368b1c3d6e
MaxAdmin <maxeduit2@gmail.com>
2022-06-06 16:48:19 +0300

    Lesson_009 03_files_arrange.py.py Step #1 - готово, код работает

--- lesson_009/03_files_arrange.py
+++ lesson_009/03_files_arrange.py
@@ -38,14 +38,13 @@ import time
 import os
 import zipfile
 
-
 class Arrange:
 
     def __init__(self, file_name):
         self.file_name = file_name
         self.file_time = None
 
-    def search(self):
+    def organize(self):
         for dirpath, dirnames, filenames in os.walk(self.file_name):
             for file in filenames:
                 full_file_path = os.path.join(dirpath, file)
@@ -54,50 +53,17 @@ class Arrange:
                 year = self.file_time[0]
                 month = self.file_time[1]
                 self.creation_dir(folder=year, subfolder=month)
-                self.copying(source=full_file_path, destination=f"icons_by_year\\{self.folder}\\{self.subfolder}")
+                folder_for_copying = f"icons_by_year\\{year}\\{month}"
+                self.copying(source=full_file_path, destination=folder_for_copying)
 
     def creation_dir(self, folder, subfolder):
-        self.folder = folder
-        self.subfolder = subfolder
-        os.makedirs(f"icons_by_year\\{self.folder}\\{self.subfolder}", exist_ok=True)
+        os.makedirs(f"icons_by_year\\{folder}\\{subfolder}", exist_ok=True)
 
     def copying(self, source, destination):
-        self.source = source
-        self.destination = destination
-        shutil.copy2(self.source, self.destination)
+        shutil.copy2(source, destination)
 
 icons = Arrange('icons')
-icons.search()
-
-shutil.copy2('icons\\animations\\process-working.png', 'icons_by_year')
-
-# zfile = zipfile.ZipFile('icons.zip', 'r')
-# for file_info in zfile.infolist():
-#     print(file_info.filename, file_info.date_time, file_info.file_size)
-
-
-
-
-# path = 'C:/Windows/help'
-# path_normalized = os.path.normpath(path)
-# print(path_normalized)
-#
-# count = 0
-# for dirpath, dirnames, filenames in os.walk(path_normalized):
-#     print('*' * 27)
-#     print(dirpath, dirnames, filenames)
-#     print(os.path.dirname(dirpath))
-#     count += len(filenames)
-#     for file in filenames:
-#         full_file_path = os.path.join(dirpath, file)
-#         secs = os.path.getmtime(full_file_path)
-#         file_time = time.gmtime(secs)
-#         if file_time[0] == 2013:
-#             # выводим только файлы за 2013 год
-#             print(full_file_path, secs, file_time)
-# print(count)
-#
-# print(__file__, os.path.dirname(__file__))
+icons.organize()
 
 
 # Усложненное задание (делать по желанию)
