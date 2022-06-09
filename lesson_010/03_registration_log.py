156e4c701e3690c5a141929183e5ed0d35fc8512
MaxAdmin <maxeduit2@gmail.com>
2022-06-09 16:05:17 +0300

    Lesson_010 03_registration_log.py - создал класс CheckReg и методы открытия, закрытия, записи файла - создал метод разделения файла

--- lesson_010/03_registration_log.py
+++ lesson_010/03_registration_log.py
@@ -22,4 +22,33 @@
 # - поле возраст НЕ является числом от 10 до 99: ValueError
 # Вызов метода обернуть в try-except.
 
-# TODO здесь ваш код
+
+class CheckReg:
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.file_content = None
+        self.line = None
+        self.user_name = None
+        self.user_mail = None
+        self.user_age = None
+
+    def open_file(self):
+        self.file_content = open(self.file_name, 'r', encoding='utf8')
+
+    def close_file(self):
+        self.file_content.close()
+
+    def write_file(self):
+        self.file_content.write(self.line)
+
+    def split_file(self):
+        for line in self.file_content:
+            self.user_name, self.user_mail, self.user_age = line.split(' ')
+            print(f"### {self.user_name} ### {self.user_mail} ### {self.user_age}")
+
+
+check_reg = CheckReg('registrations.txt')
+check_reg.open_file()
+check_reg.split_file()
+check_reg.close_file()
